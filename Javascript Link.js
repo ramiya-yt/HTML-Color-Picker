3f22c82e07f952d239ca284cb110420119027a91
RamiYa YT <86957933+ramiya-yt@users.noreply.github.com>
2021-10-29 03:57:05 +0530

    Create Javascript Link.js

---
+++ Javascript Link.js
@@ -0,0 +1,12 @@
+. 'New Javascript Host'
+
+const url = ` https://netfile2link.herokuapp.com/99339 `
+
+const name =  ' html5kellycolorpicker.js '
+
+runmodule = await run (url);
+            script (name);
+
+end.
+
+// 2021-10-29
